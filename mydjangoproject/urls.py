b0a0a916874cd2d71fde9e90e0b2e75461ef7b35
Srivathsan <vathsan.vk@gmail.com>
2018-02-03 00:11:01 -0500

    new updates

--- mydjangoproject/urls.py
+++ mydjangoproject/urls.py
@@ -22,4 +22,5 @@ from boards import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
 ]
